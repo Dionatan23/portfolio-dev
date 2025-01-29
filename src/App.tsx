15bd2881d8e8aa83d3e1f682d9ecaf2d3c9c5607
Dionatan Alves Dias <alvestavares1533@gmail.com>
2025-01-29 11:34:27 -0300

    feat: add section Copy

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Footer } from "./components/Footer";
 import { SectionContato } from "./components/SectionContatot";
 import { SectionHero } from "./components/SectionHero";
 import { SectionProjects } from "./components/SectionProjects";
+import { SectionText } from "./components/SectionText";
 
 export function App() {
   return (
     <>
       <SectionHero />
       <SectionProjects />
+      <SectionText />
       <SectionContato />
       <Footer />  
     </>
---
+++ src/components/SectionText.tsx
@@ -0,0 +1,37 @@
+export function SectionText() {
+  return (
+    <section className="w-full h-auto py-12 flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold flex items-center gap-1.5 h-10">
+        <span className="w-[34px]">🚀</span>
+        Transforme Suas Ideias em Realidade Digital
+      </h1>
+      <div className="flex flex-col gap-2.5 pl-10 pt-2.5 w-[34rem]">
+        <p className="text-sm text-justify font-normal">
+          Sou um desenvolvedor full stack especializado em criar sites modernos,
+          sistemas web e aplicações personalizadas para impulsionar negócios e
+          marcas. Com uma abordagem focada em design intuitivo, performance e
+          escalabilidade, ajudo empresas e empreendedores a destacar-se no
+          digital com soluções sob medida.
+        </p>
+        <h3 className="text-2xl font-bold mt-10">
+          💡 O que posso fazer por você?
+        </h3>
+        <p className="text-sm text-justify font-normal">
+          ✅ <b>Sites e Landing Pages de Alto Impacto</b> – Desenvolvidos com
+          React, Next.js e Tailwind CSS, garantindo design moderno e
+          carregamento rápido. <br /> <br /> ✅{" "}
+          <b>Sistemas Web Personalizados</b> – Construção de plataformas
+          robustas e seguras com Node.js, adaptadas às suas
+          necessidades. <br /> <br /> ✅ <b>Automação e Integrações</b> –
+          Conecte suas ferramentas favoritas com APIs e sistemas inteligentes
+          para otimizar seu negócio. <br /> <br />✅ <b>Aplicativos mobile</b> –
+          Com design moderno e uma UI intuitiva, criado para sua necessidades
+        </p>
+        <span className="flex text-sm text-justify font-semibold mt-2.5">
+          💬 Vamos tirar seu projeto do papel? Entre em contato e vamos
+          conversar sobre como posso te ajudar!
+        </span>
+      </div>
+    </section>
+  );
+}
