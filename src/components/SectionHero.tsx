3d63188f9ecbeb292367a124fa865b173dfa1f59
Dionatan Alves Dias <alvestavares1533@gmail.com>
2025-01-29 16:17:30 -0300

    fix: ajustes

--- src/components/SectionHero.tsx
+++ src/components/SectionHero.tsx
@@ -6,17 +6,26 @@ export function SectionHero() {
   return (
     <section className="relative flex flex-col items-center h-screen">
       <Header />
-      <img src={planeta} alt="Imagem" className="absolute top-10 left-0 w-3xs" />
+      <img
+        src={planeta}
+        alt="Imagem"
+        className="absolute top-10 left-0 w-3xs"
+      />
       <div className="m-auto w-[720px] h-[344px]">
         <p className="text-6xl font-bold">
-          Creative UI Designer Sourasith Phomhome Based in Montreal
+          Experiências visuais que cativam e impulsionam resultados!
         </p>
         <div className="flex gap-4 mt-16 mx-40">
-            <button className="w-36 h-12 bg-[#797979] rounded-md z-50 ">Hire me</button>
-            <button className="w-36 h-12 rounded-md bg-gradient-to-r from-[#C961DE] to-[#2954A3] shadow-lg]">Download CV</button>
+          <button className="w-40 h-12 rounded-md bg-gradient-to-r z-30 from-[#C961DE] to-[#2954A3] shadow-lg]">
+            Vamos Conversar
+          </button>
         </div>
       </div>
-      <img src={banner} alt="Imagem" className="absolute -bottom-30 right-0 w-[710px]" />
+      <img
+        src={banner}
+        alt="Imagem"
+        className="absolute -bottom-30 right-0 w-[710px]"
+      />
     </section>
   );
 }
